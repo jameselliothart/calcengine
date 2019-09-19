129cf0581f4e508bf9ebfe5b919a8a31156f1ea3
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-09-18 22:25:55 -0500

    implement different operations in separate classes

---
+++ src/jh/calcengine/Multiplier.java
@@ -0,0 +1,13 @@
+package jh.calcengine;
+
+public class Multiplier extends CalculateBase {
+    public Multiplier(){}
+    public Multiplier(double leftVal, double rightVal) {
+        super(leftVal, rightVal);
+    }
+    @Override
+    public void calculate() {
+        double value = getLeftVal() * getRightVal();
+        setResult(value);
+    }
+}
