316fd4af35c6ba2492c831c2219a145242b1d8e2
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-09-23 16:49:26 -0500

    refactor to extract different calculation methods from main

--- src/jh/calcengine/Main.java
+++ src/jh/calcengine/Main.java
@@ -3,30 +3,12 @@ package jh.calcengine;
 public class Main {
 
     public static void main(String[] args) {
+        useMathEquation();
+        useCalculatorBase();
 
-	    MathEquation[] equations = new MathEquation[4];
-	    equations[0] = new MathEquation( 'd', 100.0d, 50.0d);
-		equations[1] = new MathEquation('a', 25.0d, 92.0d);
-		equations[2] = new MathEquation( 's', 225.0d, 17.0d);
-		equations[3] = new MathEquation('m', 11.0d, 3.0d);
+    }
 
-		for(MathEquation equation : equations) {
-			equation.execute();
-			System.out.println(equation.getResult());
-		}
-
-		System.out.println();
-		System.out.println("Using Overloads");
-		System.out.println();
-
-		double leftDouble = 9.0d;
-		double rightDouble = 4.0d;
-		MathEquation equationOverload = new MathEquation('d');
-		equationOverload.execute(leftDouble, rightDouble);
-
-		System.out.print("result=");
-		System.out.println(equationOverload.getResult());
-
+    private static void useCalculatorBase() {
         System.out.println();
         System.out.println("Using Inheritance");
         System.out.println();
@@ -43,5 +25,30 @@ public class Main {
             System.out.print("results=");
             System.out.println(calculator.getResult());
         }
+    }
+
+    private static void useMathEquation() {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation( 'd', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation( 's', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
+
+        for(MathEquation equation : equations) {
+            equation.execute();
+            System.out.println(equation.getResult());
+        }
+
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        MathEquation equationOverload = new MathEquation('d');
+        equationOverload.execute(leftDouble, rightDouble);
+
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
     }
 }
